d022f34a08b22c5eaa23b968cc1aea7aa0478772
FelixTheCat2222 <felixlksong@gmail.com>
2025-09-08 22:20:21 +0200

    Make health bar able to change max size

---
+++ Eldritch top down game/Assets/Scripts/StatBar.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class StatBar : MonoBehaviour
+{
+    public bool jitter = false;
+    public float statValue = 50;
+    public float statMax = 100;
+    public float pixelPerUnit = 20;
+
+    public Transform bar;
+    public Transform heart;
+    public Transform bg;
+    public Transform endPiece;
+
+    /*
+    private SpriteRenderer heartRenderer;
+    
+    void Start()
+    {
+        heartRenderer = heart.GetComponent<SpriteRenderer>();
+        
+        if (heartRenderer == null)
+        {
+            Debug.LogWarning("No heart sprite renderer", gameObject);
+        }
+    }
+    */
+    
+    void Update()
+    {
+        bar.localScale = new Vector3(statValue, 1f, 1f);
+        bg.localScale = new Vector3(statMax, 1f, 1f);
+        endPiece.localPosition = new Vector3(statMax/pixelPerUnit, 0, 0);
+        
+        /*
+        Color heartColor = Color.HSVToRGB(0, 0, statValue / statMax);
+        heartRenderer.color = heartColor;
+        */
+        
+        if (jitter)
+        {
+            heart.localPosition = new Vector3(Random.Range(-0.05f, 0.05f), Random.Range(-0.05f, 0.05f), heart.position.z);
+        }
+        else
+        {
+            heart.localPosition = new Vector3(0f, 0f, heart.position.z);
+        }
+    }
+}
