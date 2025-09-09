0a70f32e0823d319cd20f24be8c2d45cbd997e50
Malthe333 <93973568+Malthe333@users.noreply.github.com>
2025-09-09 15:25:05 +0200

    Sceleton enemy

--- Eldritch top down game/Assets/Scripts/EnemyMeleeBehavior.cs
+++ Eldritch top down game/Assets/Scripts/EnemyMeleeBehavior.cs
@@ -4,21 +4,41 @@ public class EnemyMeleeBehavior : MonoBehaviour
 {
     
     private Animator animator;
-    private BoxCollider2D collider;
+    private BoxCollider2D enemy_collider;
+
+
+   [SerializeField] bool attacking = false;
+
+    public int attackDamage = 10;
+    public int health = 30;
+
     public Transform playertransform;
 
     void Start()
     {
-        collider = GetComponent<BoxCollider2D>();
+        enemy_collider = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
     }
 
     // Attack when player enters trigger
     void OnTriggerEnter2D(Collider2D other)
 {
-    if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+    int layer = other.gameObject.layer;
+
+    if (layer == LayerMask.NameToLayer("SwordHitbox"))
     {
-        animator.SetTrigger("Attack");
+        health -= 10;
+        animator.SetTrigger("IsHit");
+        Debug.Log("Enemy Health: " + health);
+        if (health <= 0)
+        {
+            animator.SetTrigger("Death");
+            Destroy(gameObject, 1f);
+        }
+    }
+    else if (layer == LayerMask.NameToLayer("Player"))
+    {
+        animator.SetTrigger("IsInAttackRange");
     }
 }
 
@@ -26,9 +46,20 @@ public class EnemyMeleeBehavior : MonoBehaviour
     void Update()
     {
 
+        if (!attacking)
+        {
+            Vector2 direction = (playertransform.position - transform.position).normalized;
+            transform.position += (Vector3)direction * Time.deltaTime;
+        }
 
-        Vector2 direction = (playertransform.position - transform.position).normalized;
-        transform.position += (Vector3)direction * Time.deltaTime;
-
+        if (transform.position.x < playertransform.position.x)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+           
+        }
+        else if (transform.position.x > playertransform.position.x)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
     }
 }
